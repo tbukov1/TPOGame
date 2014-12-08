c11dfe03bdb977f0e1ad10670637da7d5d02cd2c
Gašper Ferdin <gasper.ferdin@gmail.com>
2014-12-08 22:04:18 +0100

    narjen špil :D

--- core/src/tpo/game/TPOGame.java
+++ core/src/tpo/game/TPOGame.java
@@ -2,26 +2,34 @@ package tpo.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TPOGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();    
+        font = new BitmapFont();
+        font.setColor(Color.RED);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        font.draw(batch, "NAJHUJSI SPIL EVER!!!", 200, 200);
+        batch.end();
 	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 }
