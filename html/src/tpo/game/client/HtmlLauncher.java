ca4e66f6984c625661297adac0d35641422a3c79
Gašper Ferdin <gasper.ferdin@gmail.com>
2014-12-08 20:15:34 +0100

    dodan projekt za eclipse

---
+++ html/src/tpo/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tpo.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tpo.game.TPOGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TPOGame();
+        }
+}
