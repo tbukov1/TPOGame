47a20c73d44d58a0de32831d8c8a6c6d72107f8a
Gašper Ferdin <gasper.ferdin@gmail.com>
2014-12-09 09:57:19 +0100

    Signed-off-by: Gašper Ferdin <gasper.ferdin@gmail.com>

--- core/src/tpo/game/TPOGame.java
+++ core/src/tpo/game/TPOGame.java
@@ -4,18 +4,24 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TPOGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	BitmapFont font;
+	Sprite sprite;
+	Texture texture;
 	
 	@Override
 	public void create () {
         batch = new SpriteBatch();    
         font = new BitmapFont();
         font.setColor(Color.RED);
+        texture = new Texture(Gdx.files.internal("data/jet.png"));
+        sprite = new Sprite(texture);
 	}
 
 	@Override
@@ -25,6 +31,7 @@ public class TPOGame extends ApplicationAdapter {
         
         batch.begin();
         font.draw(batch, "NAJHUJSI SPIL EVER!!!", 200, 200);
+        sprite.draw(batch);
         batch.end();
 	}
     @Override
